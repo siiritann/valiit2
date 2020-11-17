518f277b796ce0e0a6cdfd8acfb5690d2eff5306
Siiri <siiri.tann@gmail.com>
2020-11-17 15:49:27 +0200

    Added html, css, js

---
+++ src/main/resources/public/index.js
@@ -0,0 +1,32 @@
+console.log("Seotud");
+
+let postButton = document.getElementById("createClient");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+
+postButton.addEventListener("click", function () {
+    console.log("click");
+    let client = {
+        firstName: firstName.value,
+        lastName: lastName.value
+    }
+    createClient(client).then(result => {
+
+    });
+});
+
+function createClient(client) {
+
+// Send the JavaScript company object to the server...
+    return fetch('/clients',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(client)
+        }
+    );
+
+}
+
